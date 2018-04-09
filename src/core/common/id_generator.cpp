506ff432087b3819660e17c104e3f99d68fb7aad
allanzeng <allanzeng@deepbrainchain.org>
2018-04-09 10:33:51 +0800

    add generate_id class

---
+++ src/core/common/id_generator.cpp
@@ -0,0 +1,27 @@
+#include "id_generator.h"
+
+namespace matrix
+{
+	namespace core
+	{
+		id_generator::id_generator()
+		{
+
+		}
+
+
+		id_generator::~id_generator()
+		{
+
+		}
+
+		std::string id_generator::generate_id()
+		{
+			//ref to btc
+			//todo ...
+			return std::string();
+		}
+	}
+}
+
+
