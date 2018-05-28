762575beb6c1ee07c9043643711dc384d0f5e365
reguluschen <reguluschen@deepbrainchain.org>
2018-05-28 21:05:41 +0800

    modify var name
    modify var name

--- src/api/api_call_handler.h
+++ src/api/api_call_handler.h
@@ -303,7 +303,7 @@ namespace ai
             std::string result_info;
 
             std::vector<cmd_peer_node_log> peer_node_logs;
-            std::set<char> charfilter =
+            std::set<char> char_filter =
             {
                 /*0x00,0x01,0x02,*/0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x10,0x11,0x12,0x13
                 ,0x14,0x15,0x16,0x17,0x18,0x19,0x1a,/*0x1b,*/0x1c,0x1d,0x1e,0x1f,0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27
@@ -330,7 +330,7 @@ namespace ai
                     size_t size = (it->log_content).size();
                     for (size_t i = 0; i < size; i++)
                     {
-                        if (charfilter.find(*p) != charfilter.end())
+                        if (char_filter.find(*p) != char_filter.end())
                         {
                             cout << *p;
                         }
