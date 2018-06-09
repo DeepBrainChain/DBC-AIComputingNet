e473327009353d5f421ca9d31f761210fba877fa
allanzengdbc <allanzeng@deepbrainchain.org>
2018-06-09 17:05:11 +0800

    add time_util

---
+++ src/core/common/time_util.h
@@ -0,0 +1,49 @@
+/*********************************************************************************
+*  Copyright (c) 2017-2018 DeepBrainChain core team
+*  Distributed under the MIT software license, see the accompanying
+*  file COPYING or http://www.opensource.org/licenses/mit-license.php
+* file name:        time_util.h
+* description:      time util
+* date:             2018.06.05
+* author:           Allan
+**********************************************************************************/
+
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <time.h>
+
+
+namespace matrix
+{
+    namespace core
+    {
+        class time_util
+        {
+        public:
+            static std::string time_2_str(time_t t)
+            {
+                struct tm _tm;
+                errno_t err = 0;
+#ifdef WIN32
+                err = localtime_s(&_tm, &t);
+                if (err != 0)
+                {
+                    std::cout << "get local time err: " << err << std::endl;
+                    return "";
+                }
+#else
+                localtime_r(&t, &_tm);
+#endif
+                char buf[256];
+                memset(buf, 0, __crt_countof(buf));
+                strftime(buf, __crt_countof(buf), "%Y-%M-%D %H%M%S", &_tm);
+
+                return buf;
+            }
+
+        };
+    }
+
+}
