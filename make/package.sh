cbb0988c525e55c43b5ca093ce10e99ce068969b
reguluschen <reguluschen@deepbrainchain.org>
2018-06-19 11:19:39 +0800

    add version dir

--- make/package.sh
+++ make/package.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 version=$1
 rm -rf ../package
-mkdir ../package
-cp ../output/dbc ../package/
-cp -R ../conf/ ../package/
-cp ../tool/add_dbc_user.sh ../package/
-cp ../tool/p ../package/
-cp ../tool/mining_install.sh ../package/
-cp ../tool/stopapp ../package/
-cd ../package/
-tar -cvf dbc-linux-$version.tar ./*
+mkdir -p  ../package/$version
+cd ../package/$version
+pwd
+cp ../../output/dbc ./
+cp -R ../../conf/ ./
+cp ../../tool/add_dbc_user.sh ./
+cp ../../tool/p ./
+cp ../../tool/mining_install.sh ./
+cp ../../tool/stopapp ./
+cp ../../tool/startapp ./
+chmod +x *
+cd ../
+tar -cvf dbc-linux-$version.tar $version 
 gzip *.tar
