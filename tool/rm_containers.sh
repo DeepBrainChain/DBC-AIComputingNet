721d0e8921a2a689e63c67148414cd776f7ba2f4
reguluschen <reguluschen@deepbrainchain.org>
2018-06-19 17:39:02 +0800

    rm container

--- tool/rm_containers.sh
+++ tool/rm_containers.sh
@@ -1,4 +1,9 @@
 #/bin/sh
+echo "Waring: IF you sure to delete all container please input:yes"
+read -t 30 -p ">>" param
+if [ $param != "yes" ];then
+   exit 1
+fi
 
 containers=`docker ps -a | awk '{print $1}' | grep -v CONTAINER`
 for line in $containers
