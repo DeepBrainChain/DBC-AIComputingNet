bccca09b27ba1434bb991a49eb366f86d82ed771
DBC-jeason <js@deepbrainchain.org>
2018-07-11 20:21:52 +0800

    add /bin/bash to useradd script and add inspect.sh
    Change-Id: If89271a6f489e7380c695ef2e0b988fc38f494cf

--- tool/add_dbc_user.sh
+++ tool/add_dbc_user.sh
@@ -2,13 +2,13 @@
 #set -x
 
 if [ $# -ne 1 ]; then
-	echo "params count is 1, please check params error"
-	exit
+        echo "params count is 1, please check params error"
+        exit
 fi
 
 USER_NAME=$1
 groupadd $USER_NAME
-useradd -g $USER_NAME -d /home/$USER_NAME $USER_NAME
+useradd -g $USER_NAME -d /home/$USER_NAME -s /bin/bash $USER_NAME
 mkdir /home/$USER_NAME
 chown $USER_NAME:$USER_NAME /home/$USER_NAME
 passwd $USER_NAME
