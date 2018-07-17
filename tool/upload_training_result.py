c9552ec7986867a45d8c641731f30445be94f2fb
DBC-jeason <js@deepbrainchain.org>
2018-07-17 18:34:05 +0800

    update curl ip from 45 to 243.44
    Change-Id: I3e1ca528e5e7684e8203d606ffc4deafbdac5db6

--- tool/upload_training_result.py
+++ tool/upload_training_result.py
@@ -8,7 +8,10 @@ res = api.add('/training_result_file')
 res_hash = res["Hash"]
 print "training_result_file_ipfspath is:  %s"%(res_hash)
 
-cmd = 'curl "http://114.116.19.45:8080/ipfs/%s"'%(res_hash)
+cmd = 'curl "http://122.112.243.44:5001/api/v0/get?arg=/ipfs/%s"'%(res_hash)
 (status, output) = commands.getstatusoutput(cmd)
 if status == 0:
    print("curl training_result_file_ipfspath success")
+else:
+   print("curl training result to ipfs error")
+
