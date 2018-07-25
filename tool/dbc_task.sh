690b1793f4622d866a495bc45bb748c81d2f57b4
DBC-jeason <js@deepbrainchain.org>
2018-07-25 21:13:09 +0800

    add ipfs bootstrap ip
    Change-Id: If666b215bbb3875fe34091eb52c74eedd2ad79bf

--- tool/dbc_task.sh
+++ tool/dbc_task.sh
@@ -98,7 +98,9 @@ fi
 #add ipfs bootstrap node
 ipfs bootstrap rm --all
 
-ipfs bootstrap add /ip4/114.116.19.45/tcp/4001/ipfs/QmPEDDvtGBzLWWrx2qpUfetFEFpaZFMCH9jgws5FwS8n1H
+#ipfs bootstrap add /ip4/114.116.19.45/tcp/4001/ipfs/QmPEDDvtGBzLWWrx2qpUfetFEFpaZFMCH9jgws5FwS8n1H
+ipfs bootstrap add /ip4/49.51.49.192/tcp/4001/ipfs/QmRVgowTGwm2FYhAciCgA5AHqFLWG4AvkFxv9bQcVB7m8c
+ipfs bootstrap add /ip4/49.51.49.145/tcp/4001/ipfs/QmPgyhBk3s4aC4648aCXXGigxqyR5zKnzXtteSkx8HT6K3
 ipfs bootstrap add /ip4/122.112.243.44/tcp/4001/ipfs/QmPC1D9HWpyP7e9bEYJYbRov3q2LJ35fy5QnH19nb52kd5
 
 
