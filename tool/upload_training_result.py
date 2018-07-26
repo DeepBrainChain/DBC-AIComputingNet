ede4bc043253f3ebb428427ca75f92e1cd455581
DBC-jeason <js@deepbrainchain.org>
2018-07-26 21:47:12 +0800

    update upload_training_result.py
    Change-Id: I701715f7438369489efcef445344811a1d123518

--- tool/upload_training_result.py
+++ tool/upload_training_result.py
@@ -1,15 +1,16 @@
 import ipfsapi
 import os
-import commands
+import sys
 
 api = ipfsapi.connect('127.0.0.1', 5001)
 
-res = api.add('/training_result_file')
+res = api.add(sys.argv[1])
 res_hash = res["Hash"]
 print ("training_result_file_ipfspath is:  %s"%(res_hash))
 
-cmd = 'curl "http://122.112.243.44:5001/api/v0/get?arg=/ipfs/%s"'%(res_hash)
-(status, output) = commands.getstatusoutput(cmd)
+cmd = 'curl "http://122.112.243.44:5001/api/v0/get?arg=/ipfs/%s" > /dev/null'%(res_hash)
+
+status = os.system(cmd)
 if status == 0:
    print("curl training_result_file_ipfspath success")
 else:
