58776dc3093558d52f86c9667e88307b5fe11dd7
jianmink <jianmink.taihe@gmail.com>
2018-07-31 21:41:19 +0800

    fix cli bugs: crash after key in '\'; or start task from file without read permission of current user. \n add dbc deployment
    Change-Id: Iec58edb2a95ab97b4e223a176860297cbbbd1511

---
+++ deployment/container/dbc_task.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#set -x
+
+# cp utility
+cp -f /home/dbc_utils/dbc_task_imp.sh /
+cp -f /home/dbc_utils/swarm.key /
+cp -f /home/dbc_utils/install_ipfs.sh /
+
+cp /home/dbc_utils/upload.sh /usr/bin/
+cp /home/dbc_utils/dos2unix /usr/bin/
+
+chmod +x /usr/bin/upload.sh
+chmod +x /usr/bin/dos2unix
+
+cd /
+
+echo "params: " $@
+
+if [ $# -ne 3 ]; then
+    echo "expect num of params as 3 but $#"
+    exit
+fi
+
+# install ipfs and ipfsapi
+#echo "install ipfs"
+#cp -f /home/dbc_utils/go-ipfs_v0.4.15_linux-amd64.tar.gz /opt/
+#cd /opt
+#tar xvzf ./go-ipfs_v0.4.15_linux-amd64.tar.gz
+#cd /opt/go-ipfs
+#./install.sh
+#
+#cd /
+#rm -rf /opt/go-ipfs
+#
+#echo "install ipfsapi"
+#pip install ipfsapi
+
+
+# install ipfs repo to indicated directory, e.g. /dbc/
+ipfs_install_path=/dbc/.ipfs
+
+if [ ! -d /dbc ]; then
+    mkdir /dbc
+fi
+mkdir $ipfs_install_path
+
+export IPFS_PATH=$ipfs_install_path
+
+# install ipfs repo
+cp -f /home/dbc_utils/go-ipfs_v0.4.15_linux-amd64.tar.gz /
+
+bash ./install_ipfs.sh ./go-ipfs_v0.4.15_linux-amd64.tar.gz force root $ipfs_install_path
+
+
+# install curl
+if [ ! `which curl` ]; then
+    echo "install curl"
+    apt-get update
+    apt-get install --yes curl
+fi
+
+# run ai-training task
+/bin/bash /dbc_task_imp.sh "$1" "$2" $3
+
