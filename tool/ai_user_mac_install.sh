10889f78ef1df3a9532d47dabb54e4cebe704f38
DBC-jeason <js@deepbrainchain.org>
2018-08-07 16:57:20 +0800

    add ai_user_mac_install.sh
    Change-Id: I18d6686bf6d0b2b67c39c547aafe04ec05d30fec

---
+++ tool/ai_user_mac_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#set -x
+
+release_version=0.3.3.1
+
+echo "begin to wget DBC release package"
+wget https://github.com/DeepBrainChain/deepbrainchain-release/releases/download/0.3.3.1/dbc-macos-client-0.3.3.1.tar.gz
+if [ $? -ne 0 ]; then
+    echo "***wget DBC release package failed***"
+    exit
+fi
+tar -zxvf dbc-macos-client-0.3.3.1.tar.gz
+echo "wget DBC release package finished"
+echo -e
+
+cd ./$release_version
+current_directory=`pwd`
+echo "current directory is $current_directory "
+
+echo "start to install ipfs"
+cd ./ipfs_repo/
+bash ./go-ipfs_v0.4.15_darwin-amd64.tar.gz
+cd ./../
+echo "start install ipfs finished"
+echo -e
+
+
+echo "begin to start DBC program"
+cd ./dbc_repo/
+./dbc
