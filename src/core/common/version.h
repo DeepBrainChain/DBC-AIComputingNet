048973e87be96f91315812566afecf991a53480e
reguluschen <reguluschen@deepbrainchain.org>
2018-08-07 10:24:21 +0800

    version 0.3.3.1
    Change-Id: I66e9b3818ecbdf6004a5341fbe4e233821df14d2

--- src/core/common/version.h
+++ src/core/common/version.h
@@ -11,7 +11,7 @@
 
 //major version . minor version. revision version. build version
 //version should be revised when official release
-#define CORE_VERSION                        0x00030300                       //00.03.00.00
+#define CORE_VERSION                        0x00030301                       //00.03.00.00
 #define PROTOCO_VERSION                     0x00000001
 
 #define STR_CONV(v)  #v 
