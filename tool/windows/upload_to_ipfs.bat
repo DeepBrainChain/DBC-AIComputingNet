2922848444b7edc5b80f13a4fa0534071591c6dd
allanzengdbc <allanzeng@deepbrainchain.org>
2018-08-08 01:59:15 +0800

    bugfix: get UAC when run it.
    Change-Id: Ie61ff1b2664f3b533981ff3ee53d5a1715f1ab56

--- tool/windows/upload_to_ipfs.bat
+++ tool/windows/upload_to_ipfs.bat
@@ -6,6 +6,22 @@
 :: or run upload_to_ipfs.bat directly, then input full name of file or directory;
 ::----------------------------------------
 
+::-----get UAC-----
+@echo off 
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" 
+if '%errorlevel%' NEQ '0' ( 
+goto UACPrompt 
+) else ( goto gotAdmin ) 
+:UACPrompt 
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs" 
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs" 
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B 
+:gotAdmin
+if exist "%temp%\getadmin.vbs" (del "%temp%\getadmin.vbs")
+
+::------upload process------
 @echo off
 :start
 @rem echo "check param cnt"
