650b17584bcb9aa91bf4d6bb25f90cff370ccb6e
DBC-jeason <js@deepbrainchain.org>
2018-08-27 20:38:23 +0800

    update install scipt to 0.3.4.0
    Change-Id: I5fc8685b2e9e3c6e1f8a63e69319707d71406dac

--- tool/ai_miner_install.sh
+++ tool/ai_miner_install.sh
@@ -92,9 +92,9 @@ echo 'export PATH=$PATH:$DBC_TOOL_PATH' >> ~/.bashrc
 echo "source .bashrc" >> ~/.bash_profile
 
 echo "begin to set dbc startapp auto-start when reboot/restart"
-sudo chmod 777 /etc/rc.local
-sudo echo "cd $current_directory/dbc_repo/" >> /etc/rc.local
-sudo echo 'su dbc -c "./dbc --ai --daemon"' >> /etc/rc.local
+sudo chmod +x /etc/rc.local
+sudo bash -c "echo 'cd $current_directory/dbc_repo/' >> /etc/rc.local"
+sudo bash -c "echo './dbc --ai --daemon' >> /etc/rc.local"
 echo -e
 
 echo "begin to start DBC ai-server program"
@@ -102,6 +102,7 @@ cd ./dbc_repo/
 ./dbc --ai --daemon
 cd ./../
 echo "start DBC ai-server program finished"
+
 
 echo "dbc ai user linux install finished,DBC AI server has already run as daemon mode"
 echo "run source ~/.bashrc to make DBC ENV effective"
