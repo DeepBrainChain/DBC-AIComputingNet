e442f0429b97b8ee5ee51a4c00e9f6e94a81e6e7
reguluschen <reguluschen@deepbrainchain.org>
2018-08-28 22:33:10 +0800

    update db_show
    Change-Id: I3ca64c2f8293fe4b7e4f8d30b514ad49e986faaa

--- tool/db_show/src/parse_req_db.cpp
+++ tool/db_show/src/parse_req_db.cpp
@@ -7,6 +7,7 @@ void parse_req_db(const char *path)
     leveldb::Options  options;
 
     options.create_if_missing = false;
+    options.reuse_logs = true;
     std::shared_ptr<leveldb::DB> m_req_training_task_db;
 
     try
@@ -18,9 +19,11 @@ void parse_req_db(const char *path)
             exit(0);
         }
         m_req_training_task_db.reset(db);
+        leveldb::ReadOptions r_options;
+        r_options.snapshot = db->GetSnapshot();
 
         std::unique_ptr<leveldb::Iterator> it;
-        it.reset(m_req_training_task_db->NewIterator(leveldb::ReadOptions()));
+        it.reset(m_req_training_task_db->NewIterator(r_options));
         for (it->SeekToFirst(); it->Valid(); it->Next())
         {
             //deserialization
@@ -31,6 +34,7 @@ void parse_req_db(const char *path)
             t_info.read(&proto);
             cout << t_info.task_id << "," << t_info.create_time << "," << to_training_task_status_string(t_info.status) << "," << t_info.result << endl;
         }
+        db->ReleaseSnapshot(r_options.snapshot);
     }
     catch (const std::exception & e)
     {
