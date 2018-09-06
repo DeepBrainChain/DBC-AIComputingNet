8032100ccb6fa29b9e9dd39dcd1f0c6388985802
toweryanghb <toweryang@deepbrainchain.org>
2018-09-06 17:04:49 +0800

    ut
    Change-Id: I951896fbe942c760235df0e7db8684916b765ef3

--- src/core/unittest/byte_buf_test.cpp
+++ src/core/unittest/byte_buf_test.cpp
@@ -106,10 +106,77 @@ namespace utf = boost::unit_test;
         b.read_from_byte_buf(output, sizeof(s));
 
         BOOST_TEST_MESSAGE(sizeof(s));
-
-//        BOOST_CHECK_THROW(b.write_to_byte_buf(s, l), std::length_error);
-
     }
 
+BOOST_AUTO_TEST_CASE(test_buf_to_string)
+{
+  std::shared_ptr<byte_buf> buf = std::make_shared<byte_buf>(1024);
+  char s[]="123456";
 
-//BOOST_AUTO_TEST_SUITE_END()
+  buf->write_to_byte_buf(s, sizeof(s));
+  std::string buf_string = buf->to_string();
+
+  BOOST_TEST(buf_string == "31 32 33 34 35 36 00 ");
+}
+
+BOOST_AUTO_TEST_CASE(test_get_valid_write_len)
+{
+  std::shared_ptr<byte_buf> buf = std::make_shared<byte_buf>(7);
+  char s[]="123456";
+
+  buf->write_to_byte_buf(s, sizeof(s));
+
+  uint32_t len = buf->get_valid_write_len();
+  BOOST_TEST(len == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_get_valid_write_len_1)
+{
+  std::shared_ptr<byte_buf> buf = std::make_shared<byte_buf>(2);
+  char s[]="123456";
+
+  buf->write_to_byte_buf(s, sizeof(s));
+
+  uint32_t len = buf->get_valid_write_len();
+  BOOST_TEST(len != 0);  // adjust length 2*4,valid_write_len = 8-7
+}
+
+BOOST_AUTO_TEST_CASE(test_get_valid_write_len_2)
+{
+  std::shared_ptr<byte_buf> buf = std::make_shared<byte_buf>(7);
+  char s[]="123456";
+
+  buf->write_to_byte_buf(s, sizeof(s));
+
+  uint32_t len = buf->get_valid_write_len();
+  BOOST_TEST(len == 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_move_write_ptr)
+{
+  std::shared_ptr<byte_buf> buf = std::make_shared<byte_buf>(8);
+  char s[]="123456";
+
+  buf->write_to_byte_buf(s, sizeof(s));
+
+  int32_t ret = buf->move_write_ptr(1);
+  BOOST_TEST(ret == E_SUCCESS);
+
+  BOOST_CHECK_THROW(buf->move_write_ptr(1), std::length_error);
+}
+
+BOOST_AUTO_TEST_CASE(test_move_read_ptr)
+{
+  std::shared_ptr<byte_buf> buf = std::make_shared<byte_buf>(8);
+  char s[]="123456";
+
+  buf->write_to_byte_buf(s, sizeof(s));
+
+  int32_t len = buf->read_from_byte_buf(s, sizeof(s) - 1 );
+  BOOST_TEST(len == sizeof(s) - 1 );
+
+  int32_t ret = buf->move_read_ptr(1);
+  BOOST_TEST(ret == E_SUCCESS);
+
+  BOOST_CHECK_THROW(buf->move_read_ptr(1), std::length_error);
+}
