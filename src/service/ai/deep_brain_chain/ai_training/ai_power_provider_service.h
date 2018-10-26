4554733ac613b95cef9e634629650bc60947348c
jianmink <jianmink.taihe@gmail.com>
2018-10-26 16:00:21 +0800

    split provider_service
    Change-Id: Ibd9a8854883064f33b92fa678e019b0c8294010b

--- src/service/ai/deep_brain_chain/ai_training/ai_power_provider_service.h
+++ src/service/ai/deep_brain_chain/ai_training/ai_power_provider_service.h
@@ -22,38 +22,10 @@
 #include "image_manager.h"
 #include "oss_task_manager.h"
 #include "idle_task_scheduling.h"
+#include "user_task_scheduling.h"
+#include "container_worker.h"
 
 using namespace matrix::core;
-
-
-#define AI_TRAINING_TASK_TIMER                                      "training_task"
-//#define AI_AUTH_TASK_TIMER                                          "auth_task"
-//#define AI_PULLING_IMAGE_TIMER                                          "ai_pulling_image"
-#define AI_TRAINING_TASK_TIMER_INTERVAL                 (30 * 1000)                                                 //30s timer
-#define AI_PULLING_IMAGE_TIMER_INTERVAL                 (5*3600* 1000)                                                 //5h timer
-#define AI_TRAINING_MAX_RETRY_TIMES                                  1
-#define AI_TRAINING_MAX_TASK_COUNT                                    3
-
-#define AI_TRAINING_TASK_SCRIPT_HOME                         "/home/dbc_utils/"
-#define AI_TRAINING_TASK_SCRIPT                                       "dbc_task.sh"                                            //training shell script name
-#define AI_TRAINING_BIND_LOCALTIME                                    "/etc/localtime:/etc/localtime:ro"                          //set docker container time=local host time
-#define AI_TRAINING_BIND_TIMEZONE                                    "/etc/timezone:/etc/timezone:ro"                          //set docker container time=local host time
-
-//gpu env
-#define AI_TRAINING_ENV_PATH                                            "PATH=/usr/local/nvidia/bin:/usr/local/cuda/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
-#define AI_TRAINING_LD_LIBRARY_PATH                            "LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:/usr/local/nvidia/lib:/usr/local/nvidia/lib64"
-#define AI_TRAINING_NVIDIA_VISIBLE_DEVICES              "NVIDIA_VISIBLE_DEVICES=all"
-#define AI_TRAINING_NVIDIA_DRIVER_CAPABILITIES     "NVIDIA_DRIVER_CAPABILITIES=compute,utility"
-#define AI_TRAINING_LIBRARY_PATH                                    "LIBRARY_PATH=/usr/local/cuda/lib64/stubs:"
-#define AI_TRAINING_MOUNTS_SOURCE                              "/var/lib/nvidia-docker/volumes/nvidia_driver/384.111"
-#define AI_TRAINING_MOUNTS_DESTINATION                  "/usr/local/nvidia"
-#define AI_TRAINING_MOUNTS_MODE                                  "ro"
-#define AI_TRAINING_CGROUP_PERMISSIONS                   "rwm"
-
-#define DEFAULT_SPLIT_COUNT                                                         2
-#define DEFAULT_NVIDIA_DOCKER_PORT                                                3476
-
-const bool NEED_AUTH = true;
 
 namespace image_rj = rapidjson;
 namespace bp = boost::process;
@@ -61,44 +33,9 @@ namespace ai
 {
 	namespace dbc
 	{
-
-        enum container_status
-        {
-            container_unknown = 0,
-            container_running,
-            container_closed
-        };
-
-        enum nvidia_docker_version
-        {
-            NVIDIA_DOCKER_ONE = 1,
-            NVIDIA_DOCKER_TWO = 2,
-            NVIDIA_DOCKER_UNKNOWN = 0xff,
-        };
-
-        struct task_time_stamp_comparator
-        {
-            bool operator() (const std::shared_ptr<ai_training_task> & t1, const std::shared_ptr<ai_training_task> & t2) const
-            {
-                return t1->received_time_stamp < t2->received_time_stamp;
-            }
-        };
-
-        class nvidia_config
-        {
-        public:
-
-            std::string driver_name;
-
-            std::string volume;
-
-            std::list<std::string> devices;
-        };
-
         class ai_power_provider_service : public service_module
         {
         public:
-
             ai_power_provider_service();
 
             virtual ~ai_power_provider_service() = default;
@@ -106,23 +43,17 @@ namespace ai
             virtual std::string module_name() const { return ai_power_provider_service_name; }
 
         protected:
-
-            int32_t init_conf();
-
             void init_subscription();
 
             void init_invoker();
 
             void init_timer();
 
-            int32_t init_db();
-
             int32_t service_init(bpo::variables_map &options);
 
             int32_t service_exit();
 
         protected:
-
             int32_t on_start_training_req(std::shared_ptr<message> &msg);
 
             int32_t on_stop_training_req(std::shared_ptr<message> &msg);
@@ -137,71 +68,18 @@ namespace ai
 
         protected:
             //ai power provider service
-            std::shared_ptr<nvidia_config> get_nvidia_config_from_cli();
-
-            std::shared_ptr<container_config> get_container_config(std::shared_ptr<ai_training_task> task);
-
             int32_t on_training_task_timer(std::shared_ptr<core_timer> timer);
 
-            int32_t start_exec_training_task(std::shared_ptr<ai_training_task> task);
-
-            int32_t check_training_task_status(std::shared_ptr<ai_training_task> task);
-
-            int32_t write_task_to_db(std::shared_ptr<ai_training_task> task);
-
-            int32_t load_task_from_db();
-
-            int32_t load_container();
-
-            int32_t check_cpu_config(const double & cpu_info);
-            int32_t check_memory_config(int64_t memory, int64_t memory_swap, int64_t shm_size);
-            int32_t stop_task(std::shared_ptr<ai_training_task> task, training_task_status status);
-
-
             int32_t check_sign(const std::string message, const std::string &sign, const std::string &origin_id, const std::string & sign_algo);
-            int32_t pull_image(std::shared_ptr<ai_training_task> task);
-            int32_t check_pull_image(std::shared_ptr<ai_training_task> task);
-            int32_t end_pull(std::shared_ptr<ai_training_task> task);
-            int32_t check_pull_image_state(std::shared_ptr<ai_training_task> task);
-
-            nvidia_docker_version get_nv_docker_version();
-
+       
         protected:
-
-            std::shared_ptr<leveldb::DB> m_prov_training_task_db;
-
-            std::string m_container_ip;
-
-            uint16_t m_container_port;
-
-            std::shared_ptr<container_client> m_container_client;
-
-            std::shared_ptr<container_client> m_nvidia_client;
-
-            std::unordered_map<std::string, std::shared_ptr<ai_training_task>> m_training_tasks;             //ai power provider cached training task in memory
-
-            std::list<std::shared_ptr<ai_training_task>> m_queueing_tasks;
-
             uint32_t m_training_task_timer_id;
-
-            std::shared_ptr<nvidia_config> m_nv_config;
-            variables_map m_container_args;
-
-            const int64_t m_nano_cpu = 1000000000;
-            const int64_t m_g_bytes = 1073741824;
-
-            //min disk_free 1024MB
-            const uint32_t m_min_disk_free = 1024;
-  
-            std::string m_docker_root_dir = "";
-            std::shared_ptr<image_manager> m_pull_image_mng = nullptr;
-            bool m_auto_pull_image = true;
-
-            nvidia_docker_version m_nv_docker_version;
-
             /////////////allow dbc to exec idle task, when dbc is not running ai user's task////////////////////////
             std::shared_ptr<oss_task_manager> m_oss_task_mng = nullptr;
             std::shared_ptr<idle_task_scheduling> m_idle_task_ptr = nullptr;
+
+            std::shared_ptr<user_task_scheduling> m_user_task_ptr = nullptr;
+            std::shared_ptr<container_worker> m_container_worker = nullptr;
         };
 
     }
