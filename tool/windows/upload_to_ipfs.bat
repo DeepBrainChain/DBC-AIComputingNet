afd41aaa614d9361d4ca5a78c5ebaa9b5d6b7fa1
allanzeng <allanzeng@deepbrainchain.org>
2018-11-30 22:31:34 +0800

    add bat file: init_ipfs.bat; update upload script
    Change-Id: I367828c83d84ce371e75ab6de94d049b74f94082

--- tool/windows/upload_to_ipfs.bat
+++ tool/windows/upload_to_ipfs.bat
@@ -47,20 +47,23 @@ for /f "tokens=2 delims= " %%i in (%temp%\tmp.txt) do (
 del "%temp%\tmp.txt"
 echo "hash_code: "
 echo %hash_code%
+if "%hash_code%"=="" (
+	echo "add to ipfs failed, restart ipfs may fix it."
+	goto err_exit
+)
 echo ------------------------------------------
 echo begin to upload "%file_dir%" to ipfs ......
 
 ::set IP[0]=122.112.243.44
 ::set IP[1]=18.223.4.215
 ::download bootstrap_nodes
-::echo "%temp%\bootstrap_nodes"
 "%parent_path%/tool/curl.exe" -o "bootstrap_nodes" -L "https://github.com/DeepBrainChain/deepbrainchain-release/releases/download/latest/bootstrap_nodes"
 
 set /a n=0
 for /f "tokens=2 delims=/" %%i in (bootstrap_nodes) do (	
 	::echo !n!, %%i	
 	set IP[!n!]=%%i
-	set /a n=!n!+1
+	set /a n+=1
 	)
 del "bootstrap_nodes"
 echo count of IPs: %n%
@@ -69,32 +72,27 @@ if %n% EQU 0 (
 	echo "no ipfs peer address"
 	pause > nul
 	exit
-	)
-	
+)
+
 set /a index=%random%%%%n%
 set first_idx=%index%
-if defined hash_code (
-	:checkIP
-	"%cd%/curl.exe" --connect-timeout 5 http://!IP[%index%]!:5001/api/v0/get?arg=/ipfs/%hash_code% --output nul
-	if errorlevel 1 (
-		echo !IP[%index%]! is not available.
-		set /a tmp_index=!index!+1
-		set /a index=!tmp_index!%%%n%
-		echo next ip: !index!
-		if !first_idx! equ !index! (
-			echo "no available ipfs peers"
-			echo failed to add "%file_dir%"
-			pause > nul
-			exit
-		)
-		goto checkIP	
-	) else (
-		goto succ
+:checkIP
+echo use IP[%index%]: !IP[%index%]!
+"%cd%/curl.exe" --connect-timeout 5 -Y 5120 -y 30 http:/!IP[%index%]!:5001/api/v0/get?arg=/ipfs/%hash_code% --output nul
+if errorlevel 1 (
+	echo !IP[%index%]! is not available.
+	set /a tmp_index=!index!+1
+	set /a index=!tmp_index!%%%n%
+	echo next ip: !index!
+	if !first_idx! equ !index! (
+		echo "no available ipfs peers"
+		echo failed to add "%file_dir%"
+		pause > nul
+		exit
 	)
-) else (
-	echo "add to ipfs failed, restart ipfs may fix it."
-	goto err_exit
+	goto checkIP	
 )
+
 :succ
 echo upload "%file_dir%" finished, hash_code=%hash_code%.
 echo ------------------------------------------
