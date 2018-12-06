9b2946f92c501aa2cd0915409516abe2f562ac4d
ztlmq <changshoumeng@163.com>
2018-12-06 21:15:00 +0800

    Restful api improvements using asynchronous implementations
    Change-Id: I301083216641718fa1fd97f18e3fcaf5da8a85dc

--- src/core/network/http_server.cpp
+++ src/core/network/http_server.cpp
@@ -18,15 +18,17 @@ namespace matrix
 {
     namespace core
     {
-        http_request::http_request(struct evhttp_request* req_, struct event_base* event_base_):
-                m_req(req_), m_reply_sent(false), m_event_base_ptr(event_base_)
+        http_request::http_request(struct evhttp_request* req_, struct event_base* event_base_)
+            : m_req(req_), m_reply_sent(false), m_event_base_ptr(event_base_)
         {
+
         }
 
         http_request::~http_request()
         {
-            if(!m_reply_sent)
-            {
+
+            if (!m_reply_sent) {
+
                 // Keep track of whether reply was sent to avoid request leaks
                 LOG_DEBUG << "Unhandled request";
                 reply_comm_rest_err(HTTP_INTERNAL, RPC_RESPONSE_ERROR, "Unhandled request");
@@ -45,8 +47,7 @@ namespace matrix
 
             const char* address = "";
             uint16_t port = 0;
-            if(con)
-            {
+            if (con) {
                 // evhttp retains ownership over returned address string
                 evhttp_connection_get_peer(con, (char**) &address, &port);
             }
@@ -55,22 +56,16 @@ namespace matrix
 
         http_request::REQUEST_METHOD http_request::get_request_method()
         {
-            switch(evhttp_request_get_command(m_req))
-            {
-                case EVHTTP_REQ_GET:
-                    return GET;
+            switch (evhttp_request_get_command(m_req)) {
+                case EVHTTP_REQ_GET:return GET;
                     break;
-                case EVHTTP_REQ_POST:
-                    return POST;
+                case EVHTTP_REQ_POST:return POST;
                     break;
-                case EVHTTP_REQ_HEAD:
-                    return HEAD;
+                case EVHTTP_REQ_HEAD:return HEAD;
                     break;
-                case EVHTTP_REQ_PUT:
-                    return PUT;
+                case EVHTTP_REQ_PUT:return PUT;
                     break;
-                default:
-                    return UNKNOWN;
+                default:return UNKNOWN;
                     break;
             }
         }
@@ -80,23 +75,17 @@ namespace matrix
             const struct evkeyvalq* headers = evhttp_request_get_input_headers(m_req);
             assert(headers);
             const char* val = evhttp_find_header(headers, hdr.c_str());
-            if(val)
-            {
+            if (val)
                 return std::make_pair(true, val);
-            }
             else
-            {
                 return std::make_pair(false, "");
-            }
         }
 
         std::string http_request::read_body()
         {
             struct evbuffer* buf = evhttp_request_get_input_buffer(m_req);
-            if(!buf)
-            {
+            if (!buf)
                 return "";
-            }
             size_t size = evbuffer_get_length(buf);
             /** Trivial implementation: if this is ever a performance bottleneck,
              * internal copying can be avoided in multi-segment buffers by using
@@ -105,10 +94,8 @@ namespace matrix
              * abstraction to consume the evbuffer on the fly in the parsing algorithm.
              */
             const char* data = (const char*) evbuffer_pullup(buf, size);
-            if(!data)
-            {  // returns nullptr in case of empty buffer
+            if (!data)  // returns nullptr in case of empty buffer
                 return "";
-            }
             std::string rv(data, size);
             evbuffer_drain(buf, size);
             return rv;
@@ -128,35 +115,38 @@ namespace matrix
          */
         void http_request::write_reply(int status, const std::string& reply)
         {
+
             assert(!m_reply_sent && m_req);
             // Send event to main http thread twrite_replyo send reply message
             struct evbuffer* evb = evhttp_request_get_output_buffer(m_req);
             assert(evb);
             evbuffer_add(evb, reply.data(), reply.size());
+
             auto req_copy = m_req;
             //  http_event release in http event callback function
             http_event* ev = new http_event(m_event_base_ptr, true, [req_copy, status] {
-            evhttp_send_reply(req_copy, status, nullptr, nullptr);
-            LOG_DEBUG << "send response: " << evhttp_request_get_uri(req_copy) << ", status: " << status;
 
-            // Re-enable reading from the socket. This is the second part of the libevent
-            // workaround above.
-            if(event_get_version_number() >= 0x02010600 && event_get_version_number() < 0x02020001)
-            {
-                evhttp_connection* conn = evhttp_request_get_connection(req_copy);
-                if(conn)
-                {
-                    bufferevent* bev = evhttp_connection_get_bufferevent(conn);
-                    if(bev)
-                    {
-                        bufferevent_enable(bev, EV_READ | EV_WRITE);
-                    }
-                }
-            }
+              evhttp_send_reply(req_copy, status, nullptr, nullptr);
+              LOG_DEBUG << "send response: " << evhttp_request_get_uri(req_copy) << ", status: " << status;
+
+
+              // Re-enable reading from the socket. This is the second part of the libevent
+              // workaround above.
+              if (event_get_version_number() >= 0x02010600 && event_get_version_number() < 0x02020001) {
+                  evhttp_connection* conn = evhttp_request_get_connection(req_copy);
+                  if (conn) {
+                      bufferevent* bev = evhttp_connection_get_bufferevent(conn);
+                      if (bev) {
+                          bufferevent_enable(bev, EV_READ | EV_WRITE);
+                      }
+                  }
+              }
             });
+
             ev->trigger(nullptr);
             m_reply_sent = true;
             m_req = nullptr; // transferred back to main thread
+
         }
 
         // response http request comm err
@@ -182,9 +172,7 @@ namespace matrix
             write_header("Content-Type", "application/json");
             write_reply(status, strJSON + "\r\n");
 
-
         }
-
 
         void http_request::reply_comm_rest_succ(rapidjson::Value& data)
         {
@@ -201,8 +189,7 @@ namespace matrix
 
             std::string strJSON(buffer->GetString());
 
-            if(strJSON.size() < SHORT_JSON_STRING_SIZE)
-            {
+            if (strJSON.size() < SHORT_JSON_STRING_SIZE) {
                 LOG_INFO << "response ok=> " << strJSON;
             }
 
@@ -211,60 +198,57 @@ namespace matrix
 
         }
 
-
         std::string http_request::request_method_string(REQUEST_METHOD method)
         {
-            switch(method)
-            {
-                case http_request::GET:
-                    return "GET";
+            switch (method) {
+                case http_request::GET:return "GET";
                     break;
-                case http_request::POST:
-                    return "POST";
+                case http_request::POST:return "POST";
                     break;
-                case http_request::HEAD:
-                    return "HEAD";
+                case http_request::HEAD:return "HEAD";
                     break;
-                case http_request::PUT:
-                    return "PUT";
+                case http_request::PUT:return "PUT";
                     break;
-                default:
-                    return "unknown";
+                default:return "unknown";
             }
         }
 
-        http_event::http_event(struct event_base* base, bool delete_when_triggered_,
-                const std::function<void(void)>& handler_):
-                m_delete_when_triggered(delete_when_triggered_), m_handler(handler_)
+        http_event::http_event(struct event_base* base,
+                               bool delete_when_triggered_,
+                               const std::function<void(void)>& handler_) : m_delete_when_triggered(
+            delete_when_triggered_), m_handler(handler_)
         {
             m_ev = event_new(base, -1, 0, http_event::httpevent_callback_fn, this);
             assert(m_ev);
+
         }
 
         http_event::~http_event()
         {
+
             event_free(m_ev);
+
         }
 
         void http_event::trigger(struct timeval* tv)
         {
-            if(tv == nullptr)
-            {
+
+            if (tv == nullptr)
                 event_active(m_ev, 0, 0);  // immediately trigger event in main thread
-            }
             else
                 evtimer_add(m_ev, tv);  // trigger after timeval passed
+
+
         }
 
         void http_event::httpevent_callback_fn(evutil_socket_t /**/, short /**/, void* data)
         {
             // Static handler: simply call inner handler
+
             http_event* self = static_cast<http_event*>(data);
             self->m_handler();
-            if(self->m_delete_when_triggered)
-            {
+            if (self->m_delete_when_triggered)
                 delete self;
-            }
         }
 
     }  // namespace core
