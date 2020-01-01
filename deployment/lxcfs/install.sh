33f334e140da8407121f2d21f34bdb32b60a4d16
fengbs600220 <feng@deepbrain.ai>
2020-01-01 22:40:43 +0800

    add lxcfs、jupyter passwd

--- deployment/lxcfs/install.sh
+++ deployment/lxcfs/install.sh
@@ -2,9 +2,9 @@
 mkdir -p /lib/systemd/system
 mkdir -p /usr/local/lib/lxcfs
 mkdir -p /var/lib/lxcfs
-cp lxcfs.service /lib/systemd/system/lxcfs.service
-cp lxcfs /usr/local/bin/lxcfs
-cp -rf lib/* /usr/local/lib/lxcfs/
+cp ./dbc_repo/container/lxcfs/lxcfs.service /lib/systemd/system/lxcfs.service
+cp ./dbc_repo/container/lxcfs/lxcfs /usr/local/bin/lxcfs
+cp -rf ./dbc_repo/container/lxcfs/lib/* /usr/local/lib/lxcfs/
 sudo chmod  a+x lxcfs /usr/local/bin/lxcfs
 sudo systemctl enable lxcfs.service
 sudo systemctl restart lxcfs.service
