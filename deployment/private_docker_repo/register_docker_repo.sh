92fe89e36cbe176e94bd3af1cda664d1e1c1be46
fengbs600220 <feng@deepbrain.ai>
2020-01-07 19:02:12 +0800

    update private_docker_repo

--- deployment/private_docker_repo/register_docker_repo.sh
+++ deployment/private_docker_repo/register_docker_repo.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 DIR=$(cd $( dirname "${BASH_SOURCE[0]}" ) >/dev/null && pwd)
+count=$(grep www.dbctalk.ai /etc/hosts|wc -l)
 
-repo_name="hkksros01.3322.org"
+if [ $count -eq 0 ]
+then
+    echo "111.44.254.135   www.dbctalk.ai" >> /etc/hosts
+fi
+repo_name="www.dbctalk.ai"
 repo_port=5000
 repo_crt=$DIR/domain.crt
 
 echo "register private repo $repo_name"
-
+if [ -d /etc/docker/certs.d/$repo_name:$repo_port ]
+then
+    rm -r /etc/docker/certs.d/$repo_name:$repo_port
+fi
 sudo mkdir -p /etc/docker/certs.d/$repo_name:$repo_port
 sudo cp $repo_crt /etc/docker/certs.d/$repo_name:$repo_port/
-
