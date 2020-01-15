44a7277898d636073ff1a0cee019818a4a8530fe
fengbs600220 <feng@deepbrain.ai>
2020-01-15 23:43:14 +0800

    add get running container info

--- src/service/misc/node_info_collection.cpp
+++ src/service/misc/node_info_collection.cpp
@@ -388,9 +388,9 @@ namespace service
                 return "keyword overlength";
             }
 
-            if (!m_kvs.count(k) ||k.find("container_size")== string::npos)
+            if (!m_kvs.count(k) ||k.find("container_size")== string::npos ||k.find("running")== string::npos)
             {
-                return "unknown keyword";
+                return "shit! unknown keyword";
             }
 
 
