a01c7ed984cff8df9477456be440fff0654197f4
fengbs600220 <feng@deepbrain.ai>
2020-03-16 14:51:47 +0800

    get runing tasks

--- src/service/ai/deep_brain_chain/ai_training/ai_power_provider_service.cpp
+++ src/service/ai/deep_brain_chain/ai_training/ai_power_provider_service.cpp
@@ -606,12 +606,12 @@ namespace ai
 
                 // relay on stop_training to network
                 // not support task running on multiple nodes
-                LOG_DEBUG << "ai power provider service relay broadcast stop_training req to neighbor peer nodes: " << req->body.task_id;
-                LOG_INFO << "force stop training:" << task_id << endl;
+               // LOG_DEBUG << "ai power provider service relay broadcast stop_training req to neighbor peer nodes: " << req->body.task_id;
+               // LOG_INFO << "force stop training:" << task_id << endl;
                // return  m_user_task_ptr->stop_task_only_id(task_id);//强制停止
 
 
-               // CONNECTION_MANAGER->broadcast_message(msg, msg->header.src_sid);
+                CONNECTION_MANAGER->broadcast_message(msg, msg->header.src_sid);
             }
 
             return E_SUCCESS;
