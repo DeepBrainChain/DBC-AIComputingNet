8ae227c252b4116823caa03857f96687622a020f
fengbs600220 <feng@deepbrain.ai>
2020-04-01 21:15:26 +0800

    add chain auth

--- src/core/oss/oss_client.h
+++ src/core/oss/oss_client.h
@@ -33,7 +33,7 @@ namespace matrix
             std::shared_ptr<auth_task_resp> post_auth_task(std::shared_ptr<auth_task_req> req);
             int32_t  post_auth_stop_task(std::shared_ptr<auth_task_req> req);
             int32_t post_auth_restart_task(std::shared_ptr<auth_task_req> req);
-            int32_t post_auth_start_task(std::shared_ptr<auth_task_req> req);
+            int32_t post_auth_create_task(std::shared_ptr<auth_task_req> req);
             int32_t post_auth_update_task(std::shared_ptr<auth_task_req> req);
             //idle_task
             std::shared_ptr<idle_task_resp> post_idle_task(std::shared_ptr<idle_task_req> req);
