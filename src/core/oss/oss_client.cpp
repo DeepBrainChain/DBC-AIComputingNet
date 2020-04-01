18b5e7b925f569128ea4c71c2e2e2e285425c415
fengbs600220 <feng@deepbrain.ai>
2020-04-01 20:52:47 +0800

    add chain auth

--- src/core/oss/oss_client.cpp
+++ src/core/oss/oss_client.cpp
@@ -179,6 +179,229 @@ namespace matrix
             return E_DEFAULT;
         }
 
+        //auth_stop_task
+        int32_t oss_client::post_auth_restart_task(std::shared_ptr<auth_task_req> req)
+        {
+            //req content, headers, resp
+            std::string && req_content = req->to_string_task("restart_task");
+
+            LOG_INFO << "auth req info:" << req_content;
+
+            kvs headers;
+            headers.push_back({ "Content-Type", "application/json;charset=UTF-8" });
+            //  headers.push_back({ "Host", m_http_client.get_remote_host()});
+            headers.push_back({ "Host", m_http_client.get_remote_host()});
+            LOG_INFO << "auth get_remote_host :" << m_http_client.get_remote_host() ;
+
+            http_response resp;
+            int32_t ret = E_SUCCESS;
+            std::string end_point = m_http_client.get_uri() + "/auth_task?";
+            end_point +="machine_id="+req->mining_node_id;
+            end_point +="&task_id="+req->task_id;
+            end_point +="&operation=restart_task";
+            //  std::string end_point = "https://infotest.dbchain.ai/auth_task";
+
+            LOG_INFO << "auth req end_point :" << end_point ;
+            req_content="";
+            try
+            {
+                ret = m_http_client.post_sleep(end_point, headers, req_content,resp , 30);
+
+            }
+            catch (const std::exception & e)
+            {
+                LOG_ERROR << "auth task error: " << e.what();
+                return E_DEFAULT;
+            }
+
+            if (E_SUCCESS != ret)
+            {
+                LOG_INFO << "auth failed: " << resp.body;
+                return E_DEFAULT;
+            }
+
+            LOG_INFO << "auth resp info: " << resp.body;
+
+            if (resp.body.empty()){
+                return E_DEFAULT;
+            }
+
+            rapidjson::Document doc;
+            //doc.Parse<0>(resp.body.c_str());
+            if (doc.Parse<0>(resp.body.c_str()).HasParseError())
+            {
+                LOG_ERROR << "parse inspect_container file error:" << GetParseError_En(doc.GetParseError());
+                return E_DEFAULT;
+            }
+
+
+            //message
+            if (!doc.HasMember("status"))
+            {
+                return E_DEFAULT;
+            }
+
+
+            rapidjson::Value &status = doc["status"];
+            int32_t status_int=status.GetInt();
+            LOG_INFO << "auth resp status: " << status_int;
+            if (OSS_SUCCESS_TASK ==status_int)
+            {
+                return OSS_SUCCESS_TASK;
+            }
+
+
+            return E_DEFAULT;
+        }
+
+//auth_stop_task
+        int32_t oss_client::post_auth_start_task(std::shared_ptr<auth_task_req> req)
+        {
+            //req content, headers, resp
+            std::string && req_content = req->to_string_task("start_task");
+
+            LOG_INFO << "auth req info:" << req_content;
+
+            kvs headers;
+            headers.push_back({ "Content-Type", "application/json;charset=UTF-8" });
+            //  headers.push_back({ "Host", m_http_client.get_remote_host()});
+            headers.push_back({ "Host", m_http_client.get_remote_host()});
+            LOG_INFO << "auth get_remote_host :" << m_http_client.get_remote_host() ;
+
+            http_response resp;
+            int32_t ret = E_SUCCESS;
+            std::string end_point = m_http_client.get_uri() + "/auth_task?";
+            end_point +="machine_id="+req->mining_node_id;
+
+            end_point +="&operation=start_task";
+            //  std::string end_point = "https://infotest.dbchain.ai/auth_task";
+
+            LOG_INFO << "auth req end_point :" << end_point ;
+            req_content="";
+            try
+            {
+                ret = m_http_client.post_sleep(end_point, headers, req_content,resp , 30);
+
+            }
+            catch (const std::exception & e)
+            {
+                LOG_ERROR << "auth task error: " << e.what();
+                return E_DEFAULT;
+            }
+
+            if (E_SUCCESS != ret)
+            {
+                LOG_INFO << "auth failed: " << resp.body;
+                return E_DEFAULT;
+            }
+
+            LOG_INFO << "auth resp info: " << resp.body;
+
+            if (resp.body.empty()){
+                return E_DEFAULT;
+            }
+
+            rapidjson::Document doc;
+            //doc.Parse<0>(resp.body.c_str());
+            if (doc.Parse<0>(resp.body.c_str()).HasParseError())
+            {
+                LOG_ERROR << "parse inspect_container file error:" << GetParseError_En(doc.GetParseError());
+                return E_DEFAULT;
+            }
+
+
+            //message
+            if (!doc.HasMember("status"))
+            {
+                return E_DEFAULT;
+            }
+
+
+            rapidjson::Value &status = doc["status"];
+            int32_t status_int=status.GetInt();
+            LOG_INFO << "auth resp status: " << status_int;
+            if (OSS_SUCCESS_TASK ==status_int)
+            {
+                return OSS_SUCCESS_TASK;
+            }
+
+
+            return E_DEFAULT;
+        }
+
+        int32_t oss_client::post_auth_update_task(std::shared_ptr<auth_task_req> req)
+        {
+            //req content, headers, resp
+            std::string && req_content = req->to_string_task("update_task");
+
+            LOG_INFO << "auth req info:" << req_content;
+
+            kvs headers;
+            headers.push_back({ "Content-Type", "application/json;charset=UTF-8" });
+            //  headers.push_back({ "Host", m_http_client.get_remote_host()});
+            headers.push_back({ "Host", m_http_client.get_remote_host()});
+            LOG_INFO << "auth get_remote_host :" << m_http_client.get_remote_host() ;
+
+            http_response resp;
+            int32_t ret = E_SUCCESS;
+            std::string end_point = m_http_client.get_uri() + "/auth_task?";
+            end_point +="machine_id="+req->mining_node_id;
+            end_point +="&task_id="+req->task_id;
+            end_point +="&operation=update_task";
+            //  std::string end_point = "https://infotest.dbchain.ai/auth_task";
+
+            LOG_INFO << "auth req end_point :" << end_point ;
+            req_content="";
+            try
+            {
+                ret = m_http_client.post_sleep(end_point, headers, req_content,resp , 30);
+
+            }
+            catch (const std::exception & e)
+            {
+                LOG_ERROR << "auth task error: " << e.what();
+                return E_DEFAULT;
+            }
+
+            if (E_SUCCESS != ret)
+            {
+                LOG_INFO << "auth failed: " << resp.body;
+                return E_DEFAULT;
+            }
+
+            LOG_INFO << "auth resp info: " << resp.body;
+
+            if (resp.body.empty()){
+                return E_DEFAULT;
+            }
+
+            rapidjson::Document doc;
+            //doc.Parse<0>(resp.body.c_str());
+            if (doc.Parse<0>(resp.body.c_str()).HasParseError())
+            {
+                LOG_ERROR << "parse inspect_container file error:" << GetParseError_En(doc.GetParseError());
+                return E_DEFAULT;
+            }
+
+
+            //message
+            if (!doc.HasMember("status"))
+            {
+                return E_DEFAULT;
+            }
+
+
+            rapidjson::Value &status = doc["status"];
+            int32_t status_int=status.GetInt();
+            LOG_INFO << "auth resp status: " << status_int;
+            if (OSS_SUCCESS_TASK ==status_int)
+            {
+                return OSS_SUCCESS_TASK;
+            }
+
+
+            return E_DEFAULT;
+        }
     }
 
 }
