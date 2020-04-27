abe7ba8b8723a6f0648529d6967d2d99bf3a4b02
fengbs600220 <feng@deepbrain.ai>
2020-04-27 12:09:50 +0800

    modify gpu count

--- tool/change_gpu_id.sh
+++ tool/change_gpu_id.sh
@@ -7,15 +7,15 @@ container_id=$4
 echo ${task_id}
 echo ${container_id}
 
-docker stop ${task_id}
+sudo docker stop ${task_id}
 
 users=$(ss -tnlp |grep dockerd | awk '{print $6}')
 pid_fd=$(echo ${users#*=})
 pid=$(echo ${pid_fd%,*})
 echo ${pid}
-kill -9 ${pid}
+sudo kill -9 ${pid}
 
-sed -i "s/NVIDIA_VISIBLE_DEVICES=${gpu_id_old}/NVIDIA_VISIBLE_DEVICES=${gpu_id_new}/g" /data/docker_data/containers/${container_id}/config.v2.json
+sudo sed -i "s/NVIDIA_VISIBLE_DEVICES=${gpu_id_old}/NVIDIA_VISIBLE_DEVICES=${gpu_id_new}/g" /data/docker_data/containers/${container_id}/config.v2.json
 
 sleep 10s
-docker start ${task_id}
+sudo docker start ${task_id}
