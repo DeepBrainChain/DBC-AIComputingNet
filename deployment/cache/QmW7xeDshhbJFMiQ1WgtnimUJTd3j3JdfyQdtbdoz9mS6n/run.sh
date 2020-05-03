e5389b860944ab94c97465a680a58bb2556250b3
fengbs600220 <feng@deepbrain.ai>
2020-05-03 20:43:45 +0800

    add kill jupyter

--- deployment/cache/QmW7xeDshhbJFMiQ1WgtnimUJTd3j3JdfyQdtbdoz9mS6n/run.sh
+++ deployment/cache/QmW7xeDshhbJFMiQ1WgtnimUJTd3j3JdfyQdtbdoz9mS6n/run.sh
@@ -39,7 +39,7 @@ run_jupyter()
         expect /chjupyter.exp $JUPYTER_PASSWD
     fi
 
-    ps -ef|grep jupyter-lab | awk '{print  \$2}'| sudo xargs  kill -9
+    ps -ef|grep jupyter-lab | awk '{print  $2}'| sudo xargs  kill -9
 
     sudo nohup jupyter-lab --ip 0.0.0.0 --port 8888 --no-browser --allow-root &
 }
