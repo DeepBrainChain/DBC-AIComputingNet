8b7584ad48cf07fb2717c19f0a5960be413626bf
fengbs600220 <feng@deepbrain.ai>
2020-05-16 16:06:12 +0800

    add auto restart docker

--- tool/mining_install.sh
+++ tool/mining_install.sh
@@ -49,6 +49,7 @@ sudo add-apt-repository \
        stable"
 #sudo apt-get update
 #echo y | sudo apt-get -y install docker-ce=18.06.1~ce~3-0~ubuntu
+sudo chmod 777 ./archive/containerd.io_1.2.12-1_amd64.deb  ./archive/docker-ce-cli_19.03.6~3-0~ubuntu-xenial_amd64.deb ./archive/docker-ce_19.03.6~3-0~ubuntu-xenial_amd64.deb
 sudo dpkg -i   ./archive/containerd.io_1.2.12-1_amd64.deb  ./archive/docker-ce-cli_19.03.6~3-0~ubuntu-xenial_amd64.deb ./archive/docker-ce_19.03.6~3-0~ubuntu-xenial_amd64.deb
 if [ $? -ne 0 ]; then
     echo "install docker-ce failed"
@@ -167,6 +168,8 @@ if [ $? -eq 0 ]; then
 
 
     # jimmy: install nvidia-docker 2 from local archive
+    sudo chmod 777  ./archive/libnvidia-container1_1.0.7-1_amd64.deb ./archive/libnvidia-container-tools_1.0.7-1_amd64.deb
+    sudo chmod 777  ./archive/nvidia-container-toolkit_1.0.5-1_amd64.deb ./archive/nvidia-container-runtime_3.1.4-1_amd64.deb
     sudo dpkg -i ./archive/libnvidia-container1_1.0.7-1_amd64.deb ./archive/libnvidia-container-tools_1.0.7-1_amd64.deb
     sudo dpkg -i ./archive/nvidia-container-toolkit_1.0.5-1_amd64.deb ./archive/nvidia-container-runtime_3.1.4-1_amd64.deb
     if [ $? -ne 0 ]; then
--- tool/restart_dbc.sh
+++ tool/restart_dbc.sh
@@ -4,29 +4,42 @@
 
 
 START_CMD="sudo systemctl start dbc"
-
-LOG_FILE="restart.log"
-
+START_CMD_DOCKER="sudo systemctl restart docker"
+LOG_FILE_DBC="restartdbc.log"
+LOG_FILE_DOCKER="restartdocker.log"
 
 while true 
 
 do
 
 
-   dbc=`systemctl list-units --type=service | grep "running dbc Daemon" |wc -l`
+   dbc=`systemctl list-units --type=service | grep "dbc Daemon" | awk '{print  $4}'`
 
-    if [ $dbc -eq 0 ]
+   if [ "$dbc" != "running" ]
 
     then
 
         echo "start  dbc service...................."
 
-        echo `date +%Y-%m-%d` `date +%H:%M:%S` 'restart dbc'  >>$LOG_FILE
+        echo `date +%Y-%m-%d` `date +%H:%M:%S` 'restart dbc'  >>$LOG_FILE_DBC
 
         ${START_CMD}
 
     fi
 
+   docker=`systemctl list-units --type=service | grep "Docker Application Container Engine" | awk '{print  $4}'`
+
+    if [ "$docker" != "running" ]
+
+    then
+
+        echo "restart  docker service...................."
+
+        echo `date +%Y-%m-%d` `date +%H:%M:%S` 'restart docker'  >>$LOG_FILE_DOCKER
+
+        ${START_CMD_DOCKER}
+
+    fi
 
     sleep 100s
 
