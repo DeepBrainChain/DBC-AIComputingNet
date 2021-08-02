314553f66327205f0326ce6975d22b254e3007f8
cuijg <bacuijg@163.com>
2021-08-02 17:13:25 +0800

    update

--- src/dbc/network/handler_create_functor 2.h
+++
@@ -1,9 +0,0 @@
-#ifndef DBC_NETWORK_HANDLER_CREATE_FUNCTOR_H
-#define DBC_NETWORK_HANDLER_CREATE_FUNCTOR_H
-
-#include "channel.h"
-#include "socket_channel_handler.h"
-
-typedef  std::function<std::shared_ptr<dbc::network::socket_channel_handler> (std::shared_ptr<dbc::network::channel> ch)> handler_create_functor;
-
-#endif
--- src/dbc/server/server_initiator_factory 2.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "server_initiator_factory.h"
-#include "server_initiator.h"
-
-create_functor_type server_initiator_factory::m_initiator_creator = std::function<server_initiator*()>(nullptr);
-
-server_initiator *server_initiator_factory::create_initiator()
-{
-    return m_initiator_creator();           //bind and lazy execute to avoid service invasion
-}
