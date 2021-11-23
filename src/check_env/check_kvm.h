044a6a46c3097d75d0b5a49bd42f281f21d7a2ac
Jerry <1364812552@qq.com>
2021-11-23 15:56:09 +0800

    support windows vm on check_env

--- src/check_env/check_kvm.h
+++ src/check_env/check_kvm.h
@@ -20,37 +20,9 @@ namespace check_kvm {
         std::list<std::string> devices;
     };
 
-    enum EVmStatus {
-        VS_SHUT_OFF,
-        VS_PAUSED,
-        VS_RUNNING
-    };
-
-    bool CreateDomain(const std::string& domain_name, const std::string& image,
+    std::string preCreateDomain(const std::string& domain_name, const std::string& image,
                          int32_t sockets, int32_t cores_per_socket, int32_t threads_per_core,
-                         const std::map<std::string, DeviceGpu>& mpGpu, int64_t mem);
-
-    int32_t StartDomain(const std::string& domain_name);
-
-    int32_t SuspendDomain(const std::string& domain_name);
-
-    int32_t ResumeDomain(const std::string& domain_name);
-
-    int32_t RebootDomain(const std::string& domain_name);
-
-    int32_t ShutdownDomain(const std::string& domain_name);
-
-    bool DestoryDomain(const std::string& domain_name);
-
-    int32_t ResetDomain(const std::string& domain_name);
-
-    bool UndefineDomain(const std::string& domain_name);
-
-    void ListAllDomains(std::vector<std::string>& domains);
-
-    void ListAllRunningDomains(std::vector<std::string>& domains);
-
-    EVmStatus GetDomainStatus(const std::string& domain_name);
+                         const std::map<std::string, DeviceGpu>& mpGpu, int64_t mem, int32_t vnc_port, const std::string& vnc_pwd);
 
     void test_kvm(int argc, char** argv);
 }
