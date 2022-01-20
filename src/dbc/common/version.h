892d50acf08cca95963c301076055f15821a417e
Jerry <1364812552@qq.com>
2022-01-20 17:36:33 +0800

    add version to cooperate with web front end test

--- src/dbc/common/version.h
+++ src/dbc/common/version.h
@@ -2,7 +2,7 @@
 #define DBC_VERSION_H
 
 //major version . minor version . revision version . build version
-#define CORE_VERSION                        0x00030801
+#define CORE_VERSION                        0x00030802
 #define PROTOCO_VERSION                     0x00000001
 
 #define STR_CONV(v)  #v
