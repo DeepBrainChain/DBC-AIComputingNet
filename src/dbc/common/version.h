45e93a6a706586c756f4c2226ab042c9b4037135
cuijg <bacuijg@163.com>
2022-02-17 20:06:52 +0800

    update

--- src/dbc/common/version.h
+++ src/dbc/common/version.h
@@ -2,7 +2,7 @@
 #define DBC_VERSION_H
 
 //major version . minor version . revision version . build version
-#define CORE_VERSION                        0x00030900
+#define CORE_VERSION                        0x00030901
 #define PROTOCO_VERSION                     0x00000001
 
 #define STR_CONV(v)  #v
