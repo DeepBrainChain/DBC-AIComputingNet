1beefc641e0a75098d889c3b548ff32f23718eea
Jerry <1364812552@qq.com>
2022-07-21 16:36:40 +0800

    Modify stop script

--- shell/mining/stop.sh
+++ shell/mining/stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+  
 workpath=$(cd $(dirname $0) && pwd)
 cd $workpath
 
@@ -17,4 +17,26 @@ then
     done
 fi
 
-echo "${dbc_file} stop ok!"
+echo "wait dbc process exit"
+for((i=0; i<10; i++));
+do
+    process_num=`ps -ef |grep -v grep |grep "${dbc_file}\b" | wc -l`
+    if [ ${process_num} -eq 0 ]
+    then
+        break
+    fi
+
+    echo -n "."
+    sleep 1
+done
+
+if [ ${process_num} -ne 0 ]
+then
+    pids=`ps -ef |grep -v grep |grep "${dbc_file}\b" |awk '{print $2}'`
+    for pid in $pids
+    do
+        kill -9 $pid
+    done
+fi
+
+echo -e "\n${dbc_file} stop ok!"
